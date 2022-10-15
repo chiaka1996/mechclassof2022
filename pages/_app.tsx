1a882feb415e268b4197efae4486d0b35cf84806
osuji chiaka <chiakajunior@yahoo.com>
2022-10-16 01:07:35 +0300

    checking the whole index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css'
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 import 'animate.css';
-// import {AuthProvider} from "../Contexts/AllContext"
+import {AuthProvider} from "../Contexts/AllContext"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return( 
-    // <AuthProvider>
-    // <div className={styles.overallBody}>
-    <div>
+    <AuthProvider>
+    <div className={styles.overallBody}>
   <Component {...pageProps} />
   </div>
-  // </AuthProvider>
+  </AuthProvider>
   )
 }
 
